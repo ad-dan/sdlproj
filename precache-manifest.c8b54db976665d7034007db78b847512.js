281c0f7d6e88effb41f81fe63bcb281f82e292d9
farzidev <adunreal@gmail.com>
2019-12-01 19:17:22 +0530

    Intial commit

---
+++ precache-manifest.c8b54db976665d7034007db78b847512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77217504bfb070870887b0e3b54a2aa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b5f90b4b8009df2507",
+    "url": "/static/css/main.5a873167.chunk.css"
+  },
+  {
+    "revision": "bd0ca4c09df88a233e33",
+    "url": "/static/js/2.1718c30b.chunk.js"
+  },
+  {
+    "revision": "e7b5f90b4b8009df2507",
+    "url": "/static/js/main.5d3160db.chunk.js"
+  },
+  {
+    "revision": "df15cd34d59501ca8cf9",
+    "url": "/static/js/runtime-main.678e1e69.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
